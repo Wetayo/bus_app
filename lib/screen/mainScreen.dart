986b0b93c75df89bdfa71822ff10e32c67b08514
ksssssw <rtd0731@naver.com>
2021-03-27 17:09:12 +0900

    menu update

--- lib/screen/mainScreen.dart
+++ lib/screen/mainScreen.dart
@@ -12,9 +12,78 @@ class MainScreen extends StatefulWidget {
 class _MainScreen extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        children: <Widget>[],
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(),
+      title: 'WetayoBus',
+      home: Scaffold(
+        body: Container(
+          color: Color(0xFFB1F2B36),
+          child: AppContainer(),
+        ),
+      ),
+    );
+  }
+}
+
+class AppContainer extends StatefulWidget {
+  @override
+  _AppContainState createState() => _AppContainState();
+}
+
+class _AppContainState extends State<AppContainer> {
+  final List<String> menuItems = ['Home', 'Setting'];
+  final List<String> menuIcons = ['icon_home', 'icon_settings'];
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Stack(
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              mainAxisSize: MainAxisSize.max,
+              children: <Widget>[
+                Container(
+                  child: Text('Search Bar'),
+                ),
+                Container(
+                  child: Expanded(
+                      child: new ListView.builder(
+                    itemCount: menuItems.length,
+                    itemBuilder: (context, index) => Container(
+                      child: Row(
+                        children: <Widget>[
+                          Container(
+                            padding: const EdgeInsets.all(20.0),
+                            child:
+                                Image.asset('assets/${menuIcons[index]}.png'),
+                          ),
+                          Container(
+                              padding: const EdgeInsets.all(20.0),
+                              child: Text(
+                                menuItems[index],
+                                style: TextStyle(
+                                    color: Colors.white, fontSize: 10.0),
+                              ))
+                        ],
+                      ),
+                    ),
+                  )),
+                ),
+                Container(
+                  child: Text('Logout'),
+                )
+              ],
+            ),
+          ),
+          Container(
+            color: Colors.white,
+            child: Text('HomePage'),
+          )
+        ],
       ),
     );
   }
