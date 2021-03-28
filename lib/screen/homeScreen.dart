5571324266af6414d85c88fda4679a65a9b172ea
ksssssw <rtd0731@naver.com>
2021-03-28 17:28:19 +0900

    components in HomePage update

--- lib/screen/homeScreen.dart
+++ lib/screen/homeScreen.dart
@@ -14,8 +14,67 @@ class _HomeScreenState extends State<HomeScreen> {
       child: Container(
         child: Column(
           children: <Widget>[
-            Text('HomeScreen'),
-            TimeInHourAndMinute(),
+            SizedBox(
+              height: 50.0,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                TimeInHourAndMinute(),
+                SizedBox(
+                  width: 100,
+                )
+              ],
+            ),
+            SizedBox(
+              height: 50.0,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                      color: Colors.indigo,
+                      borderRadius: BorderRadius.all(Radius.circular(15.0))),
+                  height: MediaQuery.of(context).size.height * 0.42,
+                  width: MediaQuery.of(context).size.width * 0.28,
+                ),
+                SizedBox(
+                  width: 30.0,
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                      color: Colors.indigo,
+                      borderRadius: BorderRadius.all(Radius.circular(15.0))),
+                  height: MediaQuery.of(context).size.height * 0.5,
+                  width: MediaQuery.of(context).size.width * 0.28,
+                  child: SafeArea(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Container(child: Text('Test')),
+                      ],
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 30.0,
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                      color: Colors.indigo,
+                      borderRadius: BorderRadius.all(Radius.circular(15.0))),
+                  height: MediaQuery.of(context).size.height * 0.42,
+                  width: MediaQuery.of(context).size.width * 0.28,
+                ),
+                SizedBox(
+                  width: 60.0,
+                )
+              ],
+            )
           ],
         ),
       ),
